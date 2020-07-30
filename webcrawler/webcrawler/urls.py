e2bba38f1175f2c0b22f6f2487d7cfe6de5b8d8c
Curious-Ujjwal <ujjwal24111999@gmail.com>
2020-07-30 17:30:34 +0530

    Fixed the bug

--- webcrawler/webcrawler/urls.py
+++ webcrawler/webcrawler/urls.py
@@ -1,11 +1,8 @@
-
 from django.urls import path
 from django.urls import  include
 from django.contrib import admin
-from crawler import views
 
-app_name = 'crawler'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('crawler.urls',namespace= "crawler")),
+    path('',include('crawler.urls')),
 ]
