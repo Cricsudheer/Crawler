7ecb26ad6ea07b766bfbf84e6e7a68e8e5d7ee7b
Curious-Ujjwal <ujjwal24111999@gmail.com>
2020-08-06 12:40:34 +0530

    Added urls for register

--- webcrawler/webcrawler/urls.py
+++ webcrawler/webcrawler/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from django.urls import  include
+from django.urls import include
 from django.contrib import admin
+from crawler import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('crawler.urls')),
+    path('register/',views.register,name='register')
 ]
